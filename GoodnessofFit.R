a9c0abc09f00cb5fa277001b75e00990c7bfcff1
Francesco Ungolo <73526094+ungolof@users.noreply.github.com>
2022-01-05 16:18:49 +0100

    Update GoodnessofFit.R

--- GoodnessofFit.R
+++ GoodnessofFit.R
@@ -325,6 +325,8 @@ residuals_01 <- function(observed, estimated){
 residuals_std_BSi <- function(observed, x0, delta, kappa, sigma, r){
   n_factors <- length(kappa)
   n_ages <- nrow(observed)
+  n_years <- ncol(observed)
+  
   B_tT <- matrix(NA, n_ages, n_factors)
   
   l_r1 <- log(r[1])
@@ -353,7 +355,9 @@ residuals_std_BSi <- function(observed, x0, delta, kappa, sigma, r){
 residuals_std_BSd_3F <- function(observed, x0, delta, kappa, sigma_dg, Sigma_cov, r){
   n_factors <- 3
   n_ages <- nrow(observed)
-  B_tT <- matrix(NA, n_ages, n_factors)
+  n_years <- ncol(observed)
+
+    B_tT <- matrix(NA, n_ages, n_factors)
   
   l_r1 <- log(r[1])
   l_r2 <- log(r[2])
@@ -401,7 +405,9 @@ residuals_std_BSd_3F <- function(observed, x0, delta, kappa, sigma_dg, Sigma_cov
 residuals_std_AFNSi <- function(observed, x0, delta, kappa, sigma, r){
   n_factors <- 3
   n_ages <- nrow(observed)
-  B_tT <- matrix(NA, n_ages, n_factors)
+  n_years <- ncol(observed)
+
+    B_tT <- matrix(NA, n_ages, n_factors)
   
   l_r1 <- log(r[1])
   l_r2 <- log(r[2])
@@ -411,7 +417,7 @@ residuals_std_AFNSi <- function(observed, x0, delta, kappa, sigma, r){
     B_tT[age,] <- B_AFNS(age,delta)  
   }
   
-  residuals <- observed - mu_bar_hat_AFNSi(x0, delta, kappa, sigma, r)
+  residuals <- observed - mu_bar_hat_AFNSi(x0, delta, kappa, sigma, r, observed)
   H <- meas_err_BS(l_r1, l_r2, l_rc)
 
   Var_y <- H + B_tT %*% diag(((sigma^2) / (2 * kappa)) * (1 - exp(-2 * kappa)), n_factors) %*% t(B_tT)
@@ -430,7 +436,9 @@ residuals_std_AFNSi <- function(observed, x0, delta, kappa, sigma, r){
 residuals_std_AFNSd <- function(observed, x0, delta, kappa, sigma_dg, Sigma_cov, r){
   n_factors <- 3
   n_ages <- nrow(observed)
-  B_tT <- matrix(NA, n_ages, n_factors)
+  n_years <- ncol(observed)
+
+    B_tT <- matrix(NA, n_ages, n_factors)
   
   l_r1 <- log(r[1])
   l_r2 <- log(r[2])
@@ -477,6 +485,8 @@ residuals_std_AFNSd <- function(observed, x0, delta, kappa, sigma_dg, Sigma_cov,
 residuals_std_AFGNSi <- function(observed, x0, delta1, delta2, kappa, sigma, r){
   n_factors <- 5
   n_ages <- nrow(observed)
+  n_years <- ncol(observed)
+
   B_tT <- matrix(NA, n_ages, n_factors)
   
   l_r1 <- log(r[1])
@@ -487,7 +497,7 @@ residuals_std_AFGNSi <- function(observed, x0, delta1, delta2, kappa, sigma, r){
     B_tT[age,] <- c(B_AFNS(age,delta1)[c(1,2)], B_AFNS(age,delta2)[2], B_AFNS(age,delta1)[3], B_AFNS(age,delta2)[3])
   }
   
-  residuals <- observed - mu_bar_hat_AFNSi(x0, delta1, delta2, kappa, sigma, r)
+  residuals <- observed - mu_bar_hat_AFGNSi(x0, delta1, delta2, kappa, sigma, r, observed)
   H <- meas_err_BS(l_r1, l_r2, l_rc)
   
   Var_y <- H + B_tT %*% diag(((sigma^2) / (2 * kappa)) * (1 - exp(-2 * kappa)), n_factors) %*% t(B_tT)
@@ -506,6 +516,8 @@ residuals_std_AFGNSi <- function(observed, x0, delta1, delta2, kappa, sigma, r){
 residuals_std_AFGNSd <- function(observed, x0, delta1, delta2, kappa, sigma_dg, Sigma_cov, r){
   n_factors <- 5
   n_ages <- nrow(observed)
+  n_years <- ncol(observed)
+
   B_tT <- matrix(NA, n_ages, n_factors)
   
   l_r1 <- log(r[1])
@@ -535,7 +547,7 @@ residuals_std_AFGNSd <- function(observed, x0, delta1, delta2, kappa, sigma_dg, 
     B_tT[age,] <- c(B_AFNS(age,delta1)[c(1,2)], B_AFNS(age,delta2)[2], B_AFNS(age,delta1)[3], B_AFNS(age,delta2)[3])
   }
   
-  residuals <- observed - mu_bar_hat_AFNSd(x0, delta1, delta2, kappa, sigma_dg, Sigma_cov, r, observed)
+  residuals <- observed - mu_bar_hat_AFGNSd(x0, delta1, delta2, kappa, sigma_dg, Sigma_cov, r, observed)
   H <- meas_err_BS(l_r1, l_r2, l_rc)
   Var_y <- H + B_tT %*% R_matrix %*% t(B_tT)
   
@@ -553,6 +565,8 @@ residuals_std_AFGNSd <- function(observed, x0, delta1, delta2, kappa, sigma_dg, 
 residuals_std_CIR <- function(observed, x0, delta, kappa, sigma, theta_Q, theta_P, r){
   n_factors <- length(kappa)
   n_ages <- nrow(observed)
+  n_years <- ncol(observed)
+
   B_tT <- matrix(NA, n_ages, n_factors)
   
   l_r1 <- log(r[1])
@@ -569,7 +583,7 @@ residuals_std_CIR <- function(observed, x0, delta, kappa, sigma, theta_Q, theta_
   residuals <- observed - mu_bar_hat_CIR(x0, delta, kappa, sigma, theta_Q, theta_P, r, observed)
   H <- meas_err_BS(l_r1, l_r2, l_rc)
   
-  X_t <- KF_CIR_uKD(x0, delta, kappa, sigma, theta_Q, theta_P, r)$X_t
+  X_t <- KF_CIR_uKD(x0, delta, kappa, sigma, theta_Q, theta_P, r, observed)$X_t
   P_ti <- 1e-10 * diag(1, n_factors)
   
   Var_y <- matrix(0, n_years, n_years)
@@ -610,6 +624,178 @@ avg2rates <- function(mu_bar){
     mu[i,] <- i * mu_bar[i,] - (i-1) * mu_bar[i-1,]
   }
   return(mu)
+}
+
+#=======================================================
+# - Probability of negative rates
+#=======================================================
+
+pr_neg_rates_f_BSi <- function(n_sim=100000, x0, delta, kappa, sigma, r, mu_bar, yrs_proj=1){
+  n_years <- ncol(mu_bar)
+  n_ages <- nrow(mu_bar)
+  n_factors <- length(kappa)
+  
+  A_tT <- matrix(0, n_ages, 1)
+  B_tT <- matrix(NA, n_ages, n_factors)
+  
+  X_t_last <- KF_BSi_uKD(x0, delta, kappa, sigma, r, mu_bar)$X_t[,n_years+1]
+  
+  mean_BSi <- exp(- kappa * yrs_proj) * X_t_last
+  var_BSi <- diag((sigma^2) / (2 * kappa) * (1 - exp(-2 * kappa * yrs_proj)))
+  
+  X_rnd <- matrix(NA, n_factors, n_sim)
+  
+  X_rnd <- rmvnorm(n = n_sim, mean_BSi, var_BSi)
+  
+  mu_rates_tb <- matrix(NA, n_ages, n_sim)
+  
+  for(age in 1:n_ages){    # - scroll over the ages
+    A_tT[age,1] <- A_BSi(age, sigma, delta)  
+    B_tT[age,] <- B_BSi(age, delta)  
+    for(sim in 1:n_sim){
+      mu_rates_tb[age,sim] <- A_tT[age,1] + t(B_tT[age,]) %*% X_rnd[sim,]
+    }
+  }
+  
+  mu_rates_tb_ind <- ifelse(mu_rates_tb<0,1,0)
+  
+  mu_pcg_x_BSi <- 100 * rowSums(mu_rates_tb_ind) / n_sim
+  
+  return(mu_pcg_x_BSi)
+}
+
+pr_neg_rates_f_BSd_3F <- function(n_sim=100000, x0, delta, kappa, sigma_dg, Sigma_cov, r, mu_bar, yrs_proj=1){
+  
+  n_years <- ncol(mu_bar)
+  n_ages <- nrow(mu_bar)
+  n_factors <- 3
+  
+  A_tT <- matrix(0, n_ages, 1)
+  B_tT <- matrix(NA, n_ages, n_factors)
+  
+  Sigma <- low_trg_fill_0diag(Sigma_cov) + t(low_trg_fill_0diag(Sigma_cov)) + diag(sigma_dg^2)
+  
+  X_t_last <- KF_BSd_3F_uKD(x0, delta, kappa, sigma_dg, Sigma_cov, r, mu_bar)$X_t[,n_years+1]
+  
+  R <- Sigma
+  for(row in 1:n_factors){
+    for(col in 1:n_factors){
+      R[row,col] <- Sigma[row,col] * (1 - exp(- (kappa[row] + kappa[col]) * yrs_proj)) / (kappa[row] + kappa[col])
+    }
+  }
+  
+  mean_BSd <- exp(- kappa * yrs_proj) * X_t_last
+  
+  dg_l_Sigma_chol <- cov2par(c(sigma_dg^2, Sigma_cov))$dg_l_Sigma_chol
+  odg_Sigma_chol <- cov2par(c(sigma_dg^2, Sigma_cov))$odg_Sigma_chol
+  Low_chol <- low_trg_fill_0diag(odg_Sigma_chol)
+  diag(Low_chol) <- exp(dg_l_Sigma_chol)
+  
+  delta_matrix <- low_trg_fill(delta) # - create lower diagonal matrix
+  
+  X_rnd <- matrix(NA, n_factors, n_sim)
+  
+  X_rnd <- rmvnorm(n = n_sim, mean_BSd, R)
+  
+  mu_rates_tb <- matrix(NA, n_ages, n_sim)
+  
+  for(age in 1:n_ages){    # - scroll over the ages
+    A_tT[age,1] <- A_BSd_3F(age, Low_chol, delta_matrix)
+    B_tT[age,] <- B_BSd_3F(age, delta_matrix) 
+    for(sim in 1:n_sim){
+      mu_rates_tb[age,sim] <- A_tT[age,1] + t(B_tT[age,]) %*% X_rnd[sim,]# + rnorm(1, 0, sd=sqrt(exp(rc) + exp(r1) * sum(exp(exp(r2) * c(1:age))) / age))
+    }
+  }
+  
+  mu_rates_tb_ind <- ifelse(mu_rates_tb<0,1,0)
+  
+  mu_pcg_x_BSd <- 100 * rowSums(mu_rates_tb_ind) / n_sim
+  
+  return(mu_pcg_x_BSd)
+}
+
+pr_neg_rates_f_AFNSi <- function(n_sim=100000, x0, delta, kappa, sigma, r, mu_bar, yrs_proj=1){
+  n_years <- ncol(mu_bar)
+  n_ages <- nrow(mu_bar)
+  n_factors <- 3
+  
+  A_tT <- matrix(0, n_ages, 1)
+  B_tT <- matrix(NA, n_ages, n_factors)
+  
+  X_t_last <- KF_AFNSi_uKD(x0, delta, kappa, sigma, r, mu_bar)$X_t[,n_years+1]
+  
+  mean_AFNSi <- exp(- kappa * yrs_proj) * X_t_last
+  var_AFNSi <- diag((sigma^2) / (2 * kappa) * (1 - exp(-2 * kappa * yrs_proj)))
+  
+  X_rnd <- matrix(NA, n_factors, n_sim)
+  
+  X_rnd <- rmvnorm(n = n_sim, mean_AFNSi, var_AFNSi)
+  
+  mu_rates_tb <- matrix(NA, n_ages, n_sim)
+  
+  for(age in 1:n_ages){    # - scroll over the ages
+    A_tT[age,1] <- A_AFNSi(age, sigma, delta)  
+    B_tT[age,] <- B_AFNS(age, delta)  
+    for(sim in 1:n_sim){
+      mu_rates_tb[age,sim] <- A_tT[age,1] + t(B_tT[age,]) %*% X_rnd[sim,]
+    }
+  }
+  
+  mu_rates_tb_ind <- ifelse(mu_rates_tb<0,1,0)
+  
+  mu_pcg_x_AFNSi <- 100 * rowSums(mu_rates_tb_ind) / n_sim
+  
+  return(mu_pcg_x_AFNSi)
+}
+
+pr_neg_rates_f_AFNSd <- function(n_sim=100000, x0, delta, kappa, sigma_dg, Sigma_cov, r, mu_bar, yrs_proj=1){
+  
+  n_years <- ncol(mu_bar)
+  n_ages <- nrow(mu_bar)
+  n_factors <- 3
+  
+  A_tT <- matrix(0, n_ages, 1)
+  B_tT <- matrix(NA, n_ages, n_factors)
+  
+  Sigma <- low_trg_fill_0diag(Sigma_cov) + t(low_trg_fill_0diag(Sigma_cov)) + diag(sigma_dg^2)
+  
+  X_t_last <- KF_AFNSd_uKD(x0, delta, kappa, sigma_dg, Sigma_cov, r, mu_bar)$X_t[,n_years+1]
+  
+  R <- Sigma
+  for(row in 1:n_factors){
+    for(col in 1:n_factors){
+      R[row,col] <- Sigma[row,col] * (1 - exp(- (kappa[row] + kappa[col]) * yrs_proj)) / (kappa[row] + kappa[col])
+    }
+  }
+  
+  mean_AFNSd <- exp(- kappa * yrs_proj) * X_t_last
+  
+  dg_l_Sigma_chol <- cov2par(c(sigma_dg^2, Sigma_cov))$dg_l_Sigma_chol
+  odg_Sigma_chol <- cov2par(c(sigma_dg^2, Sigma_cov))$odg_Sigma_chol
+  Low_chol <- low_trg_fill_0diag(odg_Sigma_chol)
+  diag(Low_chol) <- exp(dg_l_Sigma_chol)
+  
+  delta_matrix <- low_trg_fill(delta) # - create lower diagonal matrix
+  
+  X_rnd <- matrix(NA, n_factors, n_sim)
+  
+  X_rnd <- rmvnorm(n = n_sim, mean_AFNSd, R)
+  
+  mu_rates_tb <- matrix(NA, n_ages, n_sim)
+  
+  for(age in 1:n_ages){    # - scroll over the ages
+    A_tT[age,1] <- A_AFNSg(age, Low_chol, delta) ####### A_ind(age, exp(l_sigma), delta)  
+    B_tT[age,] <- B_AFNS(age, delta)  ###### B_ind(age,delta)  
+    for(sim in 1:n_sim){
+      mu_rates_tb[age,sim] <- A_tT[age,1] + t(B_tT[age,]) %*% X_rnd[sim,]# + rnorm(1, 0, sd=sqrt(exp(rc) + exp(r1) * sum(exp(exp(r2) * c(1:age))) / age))
+    }
+  }
+  
+  mu_rates_tb_ind <- ifelse(mu_rates_tb<0,1,0)
+  
+  mu_pcg_x_AFNSd <- 100 * rowSums(mu_rates_tb_ind) / n_sim
+  
+  return(mu_pcg_x_AFNSd)
 }
 
 
